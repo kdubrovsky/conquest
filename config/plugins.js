63b20c97dea7a7ef6054056812f3799f430c04b2
Konstantin Tereschenkov <kdubrovsky@gmail.com>
2023-02-21 22:26:18 +0400

    Switch off sprite example generation

--- config/plugins.js
+++ config/plugins.js
@@ -56,7 +56,7 @@ export default {
         mode: {
             symbol: {
                 sprite: '../icons.svg',
-                example: true
+                example: false
             }
         },
     }
